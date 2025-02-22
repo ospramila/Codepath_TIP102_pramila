f43090bf370cdcc8bb02261a738f41b3004be3c5
Pramila Yadav <pramilamy.os@gmail.com>
2025-02-22 04:48:22 -0500

    Initial commit

---
+++ Session1Cheatsheet.py
@@ -0,0 +1,20 @@
+#print(s) Prints the message s to the console
+
+print("Welcome to TIO102!")
+print(100)
+num = 101
+s ="welcome to vs"
+print(num)
+print(s)
+lst =["neha","geeta","sita"]
+print(lst)
+x =5
+y =3
+print(x+y)
+
+#len(s) Returns the length of a list or string.
+print(len(s))
+print(len(lst))
+
+
+#range(start, stop, step) Returns a sequence of numbers
